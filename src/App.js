16b1d632cdaccb39e10d97dcf42f8b85043f45dc
Dorikaa <horatiu.buricea@gmail.com>
2020-06-09 14:23:51 +0300

    add images

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const MasksPage = () => (
-  <div>
-    <h1>MASKS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/masks" component={MasksPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
