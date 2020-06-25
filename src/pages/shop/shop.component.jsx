987e2c1415fc20829f29d2b0b0d6a9ce7b1820ee
Dorikaa <horatiu.buricea@gmail.com>
2020-06-25 17:50:47 +0300

    fix a bug

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,26 +5,9 @@ import { connect } from "react-redux";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-
 import { updateCollections } from "../../redux/shop/shop.actions";
 
 class ShopPage extends React.Component {
-  unsubscribeFromSnapshot = null;
-
-  componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
-
-    collectionRef.onSnapshot(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-    });
-  }
-
   render() {
     const { match } = this.props;
     return (
